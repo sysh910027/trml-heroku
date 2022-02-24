f6460d1a5ddc1e79429827239e6c89f00033b04a
sysh910027 <100333114+sysh910027@users.noreply.github.com>
2022-02-24 12:28:36 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 const uri = process.env.MONGODB_URI;
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
