2e5a92330dcb91882059663af5897ec402a26cb7
sysh910027 <100333114+sysh910027@users.noreply.github.com>
2022-02-25 11:06:22 +0800

    Update app.js

--- app.js
+++ app.js
@@ -8,3 +8,32 @@ client.connect(err => {
   // perform actions on the collection object
   client.close();
 });
+
+// WebSocketのサーバの生成
+const ws = require('ws')
+const server = new ws.Server({port:5001});
+
+// 接続時に呼ばれる
+server.on('connection', ws => {
+    // クライアントからのデータ受信時に呼ばれる
+    ws.on('message', message => {
+        message=message.split(";");
+        let correct=verifyAns(message[1]);
+        // クライアントにデータを返信
+        server.clients.forEach(client => {
+            client.send(message[0]+"; "+correct);
+        });
+    });
+
+    // 切断時に呼ばれる
+    ws.on('close', () => {
+        console.log('close');
+    });
+});
+
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(80);
